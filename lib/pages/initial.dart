79d52307d416be7f841786940d883f998f71b777
Gabriel <gabrielnfa999@gmail.com>
2022-06-11 15:38:42 -0300

    new project

---
+++ lib/pages/initial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Initial extends StatefulWidget {
+  const Initial({Key? key}) : super(key: key);
+
+  @override
+  State<Initial> createState() => _InitialState();
+}
+
+class _InitialState extends State<Initial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
